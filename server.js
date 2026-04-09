90431587de327f57bb39cfe665cb6fe06e553327
soan22541 <sosotag@ik.me>
2026-04-09 16:45:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
 
+app.use(express.json());
+
+let players = {};
+
+app.post('/api/player', (req, res) => {
+    players[req.body.username] = req.body;
+    res.send({ ok: true });
+});
+
+app.get('/api/player/:username', (req, res) => {
+    res.send(players[req.params.username] || {});
+});
+
+app.listen(3000, () => console.log("API online"));
